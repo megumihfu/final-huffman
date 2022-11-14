37dac2d36999383c8e919433bd848710a1aee85d
megumihfu <99253153+megumihfu@users.noreply.github.com>
2022-11-15 00:01:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,22 +11,26 @@ int main()
     Occurency *occ;
     occ = init_occ(filename); 
     occ = reading_file(occ, filename);
-    
-    set_counter(occ); //set the counter of the array text and occ_text 
-    printf("\nsize for the new tab is %d\n", occ->counter);
-    
+
     ListeNoeud *Arbre;
     Arbre = initListeNoe();
     remplirChaine(Arbre, occ);   
     Arbre->premier = triElements(Arbre->premier);
+
+    printf("counter is : %d\n", occ->counter);
     
     Arbre->premier = huff(Arbre->premier);
-    afficherListe(Arbre);
+    //afficherListe(Arbre);
+    create_code1(occ, Arbre->premier);
+    printf("first code is done\n");
     occ = create_code2(occ, Arbre->premier, Temp);
     compression(occ, filename); //compress the file using the dictionnary from the binary tree
-
+    
+    set_counter(occ); //set the counter of the array text and occ_text 
+    printf("\nsize for the new tab is %d\n", occ->counter);
+    
     for(int i = 0; i < occ->counter; i++){ //test 
-        printf("cara %c prend code %s et apparait %d fois\n", occ->text[i], occ->code[i], occ->occ_text[i]);
+        printf("car %c = code %s et son occ est %d\n", occ->text[i], occ->code[i], occ->occ_text[i]);
         //printf("caracter : %c\n", occ->text[i]);
         //printf("occurency : %d\n", occ->occ_text[i]);
         //printf("\n");
