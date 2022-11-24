1d47d5e3e08f3d4e494a20328ff571ced991834d
megumihfu <99253153+megumihfu@users.noreply.github.com>
2022-11-24 19:59:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,220 +2,72 @@
 
 int main()
 {
-    char Temp[20], filename[20], fileName[20];
-    int size, choix1, choix2, choix3;
-    //char *code1, *tmp;
+    char *code1;
+    FILE *f;
+    f = fopen("compressed.txt", "r");
 
-    //printf("Please enter the file you want : ");
-    //scanf("%s", filename);
+    Occurency *occ;
+    occ = init(); 
+    char Temp[20];
+    occ = reading_file(occ);
+    size(occ); //on donne la nouvelle taille au compteur 
+    printf("\nnbr : %d\n", occ->counter);
     
-    FILE *fp; //pointer for the file where is our text
-    FILE *f = fopen("compressed.txt", "a");  //pointer for the compressed file
-    FILE *fd = fopen("decompressed.txt", "a");//pointer for the decompressed file
-    //fp = fopen(filename, "r");
-    //f = fopen("compressed.txt", "a");
-    //fd = fopen("decompressed.txt", "a");
-
-/*initialisation*/
-    Dictionnary *dico;
-    dico = init_dico(filename); 
     ListeNoeud *Arbre;
     Arbre = initListeNoe();
-
-while(1){
-    choix1 = menu();
-
-    switch(choix1)
-    {
-        case 1 : 
-            printf("\nSouhaitez vous compresser votre fichier [1] ou le décompresser [2]?\n");
-            scanf("%d", &choix2);
-
-            if(choix2 == 1)
-            {  
-                printf("\nVeuillez saisir le nom du fichier de votre choix : ");
-                scanf("%s", filename);
-
-                fp = fopen(filename, "r");
-                    if(fp == NULL || f == NULL)
-                        printf("Error, we cannot open your file\n");
-
-                dico = reading_file(dico, fp);
-                remplirChaine(Arbre, dico);   
-                Arbre->premier = triElements(Arbre->premier);
-                Arbre->premier = huff(Arbre->premier);
-                size = file_size(fp);
-                printf("file size if %d\n", size);
-                fprintf(f, "%d\n", size);
-                
-                fprintf(f, "1"); //because first node is always 1 in the tree
-                create_code1(dico, Arbre->premier, f);
-                fprintf(f, "\n");
-
-                dico = create_code2(dico, Arbre->premier, Temp); 
-                printf("\nsize for the new tab is %d\n", dico->counter);
-                compression(dico, filename, fp, f);   
-
-                printf("Votre compression s'est bien effectuée. Vous allez être dirigé vers le menu\n");
-                sleep(2);
-                fclose(fp);
-                //fclose(f);
-                //system("clear");
-            }
-
-            if(choix2 == 2)
-            {
-                //appeler la fonction de décompression
-                printf("Votre décompression s'est bien effectuée. Vous allez être dirigé vers le menu\n");
-                sleep(2);
-                system("clear");
-            }            
-            break;
-
-        case 2 : 
-            system("clear");
-            freopen("compressed.txt", "r", f); //and w if u want to erase the data in it
-            freopen("decompressed.txt", "r", fd);
-            fp = fopen("file.txt", "r"); 
-
-            printf("Vous pouvez afficher les fichiers txt suivants :\n");
-            printf("- file.txt --------------- 1\n");
-            printf("- compressed.txt --------- 2\n");
-            printf("- decompressed.txt ------- 3\n");
-            printf("Quel est votre choix ? ");
-            scanf("%d", &choix3);
-
-            switch (choix3){
-                case 1 :
-                    system("clear");
-                    display_file(fp); //display file
-                    return 0;
-                break;
-
-                case 2 :
-                    system("clear");
-                    display_file(f); //display compressed file
-                    return 0;
-                break;
-
-                case 3 :
-                    system("clear");
-                    display_file(fd); //display decompressed file
-                    return 0;
-                break;
-            }
-        break;
-
-        default :
-            system("clear"); 
-            printf("Vous allez quitter le programme\nFermeture des fichiers en cours...");
-            sleep(2);
-            freopen("compressed.txt", "w", f); //on efface le contenu des fichiers de compression et decompression
-            freopen("decompressed.txt", "w", fd); 
-            fclose(f); //on ferme nos fichiers ouverts
-            fclose(fp);
-            fclose(fd);
-        return 0;      
-    }
-}    
-    /*Dictionnary *dico;
-    dico = init_dico(filename); 
-    dico = reading_file(dico, fp);
-
-    ListeNoeud *Arbre;
-    Arbre = initListeNoe();
-    remplirChaine(Arbre, dico);   
+    remplirChaine(Arbre, occ);   
     Arbre->premier = triElements(Arbre->premier);
-    Arbre->premier = huff(Arbre->premier);
-    //printf("oii my boiiii\n");
-    //afficherListe(Arbre);
-    //create_code1(dico, Arbre->premier);
-
-    size = file_size(fp);
-    printf("file size if %d\n", size);
-    fprintf(f, "%d\n", size);
     
-    fprintf(f, "1"); //because first node is always 1 in the tree
-    create_code1(dico, Arbre->premier, f);
-    fprintf(f, "\n");
-
-    dico = create_code2(dico, Arbre->premier, Temp); 
-    printf("\nsize for the new tab is %d\n", dico->counter);
-    compression(dico, filename, fp, f);   
-    //printf("our actual counter is : %d'''\n", dico->counter);
-    //fclose(f);
-    */
-
-//DECOMPRESSION
- /*  freopen("compressed.txt", "r", f);
-    if(f == NULL)
-        printf("Error trying to reopen the compressed file with reading mode\n");
-
-    dico = data_from_file(dico, f);
-    //dico = set_counter(dico);
-    printf("COUNTER IS SET AT : %d\n", dico->counter);
-    //strcpy(code1, dico->dec);
-
-    for(int i = 0; i < dico->counter; i++){
-        code1[i] = dico->dec[i];
-        printf("code1[%d] = %c | ", i, code1[i]);
-        printf("dico->[%d] = %c\n", i, dico->dec[i]);
+    Arbre->premier = huff(Arbre->premier);
+    //afficherListe(Arbre);
+    occ = create_code2(occ, Arbre->premier, Temp);
+    int i = 0;
+    while(occ->text[i] != '\0')
+    {
+        printf("compteur : %d -- ", i);
+        printf("on a le caractere %c --> %s\n", occ->text[i], occ->code[i]);
+        
+        i++;
     }
+    //compression(occ);
+    //printf("1");
+    create_code1(occ, Arbre->premier);
+    printf("\n");
 
+    //printf("nbr of node is : %d\n", count_node(Arbre->premier));
+   
     ListeNoeud *newArbre;    
     newArbre = initListeNoe();
-
-    //printf("our actual counter is : %d''''\n", dico->counter);
     
-    Noeud *prem = malloc(sizeof(Noeud));
-    prem->accroche = NULL;
-    prem->branche_droite = NULL;
-    prem->branche_gauche = NULL;
-    prem->valeur = '\0';
-    newArbre->premier = prem;
+    Noeud *prems = malloc(sizeof (Noeud));
+    prems->accroche = NULL;
+    prems->branche_droite = NULL;
+    prems->branche_gauche = NULL;
+    prems->valeur = '\0';
 
-    creerAbrDec(prem, code1);
-    printf("lui %c\n", prem->branche_gauche->branche_droite->branche_droite->valeur);
+    newArbre->premier = prems;
 
-    /*strcpy(code1, dico->dec);
-    dico = set_counter(dico); 
-    printf("dico->counter %d\n", dico->counter);
-    //char *code1 = {"10A110R0P10B0T"};
-    for(int i = 0; i < SIZE; i++){
+    occ = data_from_file(occ, f);  //bug ici !!!!
+    printf("counter is : %d\n", occ->counter);
+
+    //occ->dec = (int *)malloc(occ->counter * sizeof(int));
+    code1 = (int *)malloc(occ->counter * sizeof(int));
+    
+    for(int i = 0; i < occ->counter; i++){
         code1[i] = 0;
     }
 
-    for(int i = 0; i < 30; i++){
-       // printf("code1[%d] = %c\n", i, code1[i]);
-        printf("dico->[%d] = %c\n", i, dico->dec[i]);
-    }
-   //strcpy(code1, dico->dec);
-
-    for(int i = 0; i < 13; i++){
+    strcpy(code1, occ->dec);
+    for(int i = 0; i < occ->counter; i++){
         printf("code1[%d] = %c\n", i, code1[i]);
     }
 
-    ListeNoeud *newArbre;    
-    newArbre = initListeNoe();
+    //char *entete = {"10A110R0P10B0T"};
+    creerAbrDec(prems, code1);
+    printf("lui %c\n", prems->branche_gauche->branche_droite->branche_droite->valeur);
 
-    //printf("our actual counter is : %d''''\n", dico->counter);
-    
-    Noeud *prem = malloc(sizeof(Noeud));
-    prem->accroche = NULL;
-    prem->branche_droite = NULL;
-    prem->branche_gauche = NULL;
-    prem->valeur = '\0';
-    newArbre->premier = prem;
-
-    creerAbrDec(prem, code1);
-    printf("lui %c\n", prem->branche_gauche->branche_droite->branche_droite->valeur);           */
-
-    //printf("our actual counter is : %d'''''\n", dico->counter);
-
-
-    fclose(fp);
-    //freopen("compressed.txt", "w", f); //when u dont need the value that were already compressed
-    fclose(fd);
+    free(code1);
     fclose(f);
+
     return 0;
 }
